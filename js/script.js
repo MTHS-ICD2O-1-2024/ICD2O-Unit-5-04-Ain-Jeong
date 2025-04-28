bc1d641070f3378c0dc4de8290203b481d93db70
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-04-28 18:21:07 +0000

    FINALLY

--- js/script.js
+++ js/script.js
@@ -8,26 +8,22 @@
  * This function check user's age and the type of movie they can watch
  */
 // eslint-disable-next-line no-unused-vars
-function checkAge () {
+function checkPrice() {
   // input
-  const userAge = parseFloat(document.getElementById('user-age').value)
+  const userAge = parseFloat(document.getElementById("user-age").value)
+  const day = document.getElementById("day").value
 
   // process
-  if (userAge >= 17) {
+  if (
+    (userAge <= 21 && userAge >= 12) ||
+    day === "tuesday" ||
+    day === "thursday"
+  ) {
     // output
-    document.getElementById('answer').innerHTML =
-      'You can watch R movies alone.'
-  } else if (userAge >= 13) {
-    // output
-    document.getElementById('answer').innerHTML =
-      'You can watch PG-13 movies alone.'
-  } else if (userAge >= 5) {
-    // output
-    document.getElementById('answer').innerHTML =
-      'You can watch G or PG movies alone.'
+    document.getElementById("answer").innerHTML =
+      "You are eligible for the student pricing!"
   } else {
     // output
-    document.getElementById('answer').innerHTML =
-      'You are too young for most of the movies.'
+    document.getElementById("answer").innerHTML = "You must pay regular price."
   }
 }
